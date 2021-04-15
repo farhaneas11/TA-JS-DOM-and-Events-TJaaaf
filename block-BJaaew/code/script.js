1955e3fedb28cdaa92a356e4808e11d5ccf7a3a3
farhaneas11 <farhan.salam11@gmail.com>
2021-04-15 22:03:07 +0530

    js6

--- block-BJaaew/code/script.js
+++ block-BJaaew/code/script.js
@@ -1,16 +1,35 @@
 let ul = document.querySelector('.wrap');
-let input = document.getElementById('#search');
-
+let input = document.querySelector('#search');
+let select = document.querySelector('.names');
 
 let peopleAll = got.houses.reduce((acc,cv)=>{
     acc = acc.concat(cv.people);
     return acc;
 },[]);
 
+let allHouse = got.houses.map((p)=>p.name);
+let activehouse = "";
+
+function createTag(tags = []){
+    select.innerHTML = "";
+    tags.forEach((tag)=>{
+        let li = document.createElement('li');
+        li.innerText = tag;
+        li.addEventListener("click",()=> {
+            activehouse = tag;
+            let peoplehouse = got.houses.find((house)=>house.name === tag ).people || [];
+            createcards(peoplehouse);
+        })
+        select.append(li);
+    })
+}
+createTag(allHouse);
+
+
 
 function createcards(data =[]){
     ul.innerHTML  = "";
-    peopleAll.forEach((person)=> {
+    data.forEach((person)=> {
         let li = document.createElement('li');
         let img = document.createElement('img');
         img.alt = person.name;
@@ -33,7 +52,10 @@ createcards(peopleAll);
 
 function searchpeople(event){
     let searchtext = event.target.value;
-    
+    let filtpeople = peopleAll.filter((er) =>
+     er.name.toLowerCase().inlcudes(searchtext.toLowerCase())
+     );
+    createcards(filtpeople);
 }
 input.addEventListener("input",searchpeople);
 
